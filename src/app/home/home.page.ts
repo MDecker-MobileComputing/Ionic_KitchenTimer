2e89ad3a41094758a649e69d20a150378335b815
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-05-01 23:11:12 +0200

    Button-Event-Handler mit Toast-Anzeige implementiert ... aber Notis werden noch nicht eingeplant

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  /**
+   * Konstruktor für *Dependency Injection*.
+   */  
+  constructor(private toastController: ToastController) {}
+
+  /**
+   * Event-Handler-Methode für Buttons um Timer einzuplanen.
+   * 
+   * @param minutes Laufzeit des Timers
+   */
+  public async onBegruessenButton(minutes: number) {
+
+    this.zeigeToast(`Timer mit ${minutes} Minuten Laufzeit gestartet.`);
+  }
+
+  /**
+   * Hilfsmethode für Anzeige eines Toasts.
+   * 
+   * @param nachricht In Toast anzuzeigender Text
+   */
+  private async zeigeToast(nachricht: string) {
+
+    const toast = await this.toastController.create({    
+      message : nachricht,
+      duration: 2000  // 2000 ms = 2 seconds
+    });
+
+    await toast.present();
+  }
 
 }
